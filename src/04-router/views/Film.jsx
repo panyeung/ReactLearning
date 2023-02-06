5c4f1307b41dc0da932b1bc04a53e827898d5a1a
Pan Yeung <panyeung2015@gmail.com>
2023-02-06 07:30:56 -0600

    NestedRoute

--- src/04-router/views/Film.jsx
+++ src/04-router/views/Film.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import { Redirect, Route, Switch } from "react-router-dom";
+import CommingSoon from "./Films/CommingSoon";
+import NowPlaying from "./Films/NowPlaying";
 export default function Film() {
-  return <div>Film</div>;
+  return (
+    <div>
+      Film
+      {/* NestedRoute */}
+      <Switch>
+        <Route path="/films/commingSoon" component={CommingSoon}></Route>
+        <Route path="/films/nowPlaying" component={NowPlaying}></Route>
+        <Redirect from="/films" to="/films/nowPlaying"></Redirect>
+      </Switch>
+    </div>
+  );
 }
